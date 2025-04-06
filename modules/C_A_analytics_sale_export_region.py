663cdf8de664fddea73256abeb0561e6bbdef21c
mybear1109 <mybear8211@gmail.com>
2025-04-06 18:02:59 +0900

    Update C_A_analytics_sale_export_region.py

--- modules/C_A_analytics_sale_export_region.py
+++ modules/C_A_analytics_sale_export_region.py
@@ -14,3 +14,10 @@ from datetime import datetime, timedelta
 import urllib3
 import re
 
+def export_region_ui():
+    st.subheader("판매·수출 관리")
+    st.write("해외 판매 실적을 분석하는 페이지입니다.")
+    st.write("수출입 국가별 실적 분석")
+    st.write("해외 시장 비교")
+
+
