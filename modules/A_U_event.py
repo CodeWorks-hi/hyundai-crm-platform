c86e15d78d0d30076defdd5fe914a8d86d41a3c7
nhchoi2 <choi1278@gmail.com>
2025-04-06 17:40:29 +0900

    1

--- modules/A_U_event.py
+++ modules/A_U_event.py
@@ -10,14 +10,15 @@ def event_ui():
             <p style="font-size: 18px;">고객님을 위한 스페셜 이벤트는 계속됩니다. 즐거운 행운과 경품을 만나보세요!</p>
         </div>
     """, unsafe_allow_html=True)
-    
+    st.markdown("")
+
     col4, col1, col2, col3, col5  = st.columns([0.1, 1, 0.1, 1, 0.1])
     with col4:
         pass
     with col5:
         pass
     with col1:
-        st.image("https://www.nanamcom.co.kr/news/photo/202403/7630_19015_5241.png", use_container_width=True)
+        st.image("images/event/event4.png", use_container_width=True)
         st.markdown("#### 현대카 클럽 프로그램")
         st.caption("장기 이용 고객을 위한 프리미엄 프로그램! 50개월 이상 사용 후 차량 반납 시 중고차 시세 보상과 신차 구매금액의 5%를 지원해드립니다.")
         with st.expander("프로그램 - 상세보기"):
@@ -33,7 +34,7 @@ def event_ui():
         pass
 
     with col3:
-        st.image("https://cdn.fnnews21.com/news/photo/201306/6165_5290_3630.jpg", use_container_width=True)
+        st.image("images/event/event3.jpg", use_container_width=True)
         st.markdown("#### 현대카드 제휴 이벤트")
         st.caption("현대카드로 차량을 구매하면 슈퍼콘서트 티켓 2매 증정! 자동차와 함께 특별한 경험까지 함께 드립니다.")
         with st.expander("이벤트 - 상세보기"):
@@ -42,6 +43,40 @@ def event_ui():
                 - **혜택**: 현대카드 슈퍼콘서트 티켓 2매 증정  
                 - **참여 방법**: 차량 구매 시 현대카드 사용
             """)
-
-
+    st.markdown("---")
+    cola, colb, colc, cold, cole  = st.columns([2, 0.1, 2, 0.1, 2])
+    with colb:
+        pass
+    with cold:
+        pass
+    with cola:
+        st.image("images/event/event5.png", use_container_width=True)
+        st.markdown("#### 현대카드 제휴 이벤트")
+        st.caption("현대카드로 차량을 구매하면 슈퍼콘서트 티켓 2매 증정! 자동차와 함께 특별한 경험까지 함께 드립니다.")
+        with st.expander("이벤트 - 상세보기"):
+            st.markdown("""
+                - **대상**: 현대카드로 차량 구매한 고객  
+                - **혜택**: 현대카드 슈퍼콘서트 티켓 2매 증정  
+                - **참여 방법**: 차량 구매 시 현대카드 사용
+            """)
+    with colc:
+        st.image("images/event/event6.png", use_container_width=True)
+        st.markdown("#### 현대카드 제휴 이벤트")
+        st.caption("현대카드로 차량을 구매하면 슈퍼콘서트 티켓 2매 증정! 자동차와 함께 특별한 경험까지 함께 드립니다.")
+        with st.expander("이벤트 - 상세보기"):
+            st.markdown("""
+                - **대상**: 현대카드로 차량 구매한 고객  
+                - **혜택**: 현대카드 슈퍼콘서트 티켓 2매 증정  
+                - **참여 방법**: 차량 구매 시 현대카드 사용
+            """)
+    with cole:
+        st.image("images/event/event7.png", use_container_width=True)
+        st.markdown("#### 현대카드 제휴 이벤트")
+        st.caption("현대카드로 차량을 구매하면 슈퍼콘서트 티켓 2매 증정! 자동차와 함께 특별한 경험까지 함께 드립니다.")
+        with st.expander("이벤트 - 상세보기"):
+            st.markdown("""
+                - **대상**: 현대카드로 차량 구매한 고객  
+                - **혜택**: 현대카드 슈퍼콘서트 티켓 2매 증정  
+                - **참여 방법**: 차량 구매 시 현대카드 사용
+            """)
 
