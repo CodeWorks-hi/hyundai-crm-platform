b74d92afcb0af13c757bbebd688a73146500a2b6
nhchoi2 <choi1278@gmail.com>
2025-04-07 18:15:42 +0900

    1

--- modules/A_U_consult.py
+++ modules/A_U_consult.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 import pandas as pd
-
+from datetime import datetime
 
 def consult_ui():
     if "wait_page" not in st.session_state: st.session_state["wait_page"] = 0
@@ -13,31 +13,29 @@ def consult_ui():
             return name[0] + "*" + name[-1]
         return name
 
+    df_path = "data/consult_log.csv"
+    os.makedirs("data", exist_ok=True)
+    if os.path.exists(df_path):
+        df = pd.read_csv(df_path)
+    else:
+        df = pd.DataFrame(columns=["이름", "전화번호", "상담날짜", "상담시간", "요청사항", "담당직원", "완료여부", "답변내용", "상담내용", "상담태그", "고객피드백", "목적"])
+
     left_form, right_form = st.columns(2)
 
     with left_form:
         with st.expander("방문 예약", expanded=True):
             with st.form("consult_form", clear_on_submit=True):
                 col1, col2 = st.columns(2)
-                with col1:
-                    name = st.text_input("이름")
-                with col2:
-                    phone = st.text_input("전화번호")
+                name = col1.text_input("이름")
+                phone = col2.text_input("전화번호")
 
                 col3, col4 = st.columns(2)
-                with col3:
-                    date = st.date_input("희망 상담 날짜")
-                with col4:
-                    time = st.time_input("희망 상담 시간")
+                date = col3.date_input("희망 상담 날짜")
+                time = col4.time_input("희망 상담 시간")
 
-                content = st.text_area("상담 내용")
+                content = st.text_area("상담 내용") or "-"
 
-                if content is None:
-                    content = "-"
-
-                submitted = st.form_submit_button("예약하기")
-
-                if submitted:
+                if st.form_submit_button("예약하기"):
                     new_data = {
                         "이름": name,
                         "전화번호": phone,
@@ -52,14 +50,7 @@ def consult_ui():
                         "고객피드백": "-",
                         "목적": "방문"
                     }
-
-                    df_path = "data/consult_log.csv"
-                    if os.path.exists(df_path):
-                        df = pd.read_csv(df_path)
-                        df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
-                    else:
-                        df = pd.DataFrame([new_data])
-
+                    df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
                     df.to_csv(df_path, index=False)
                     st.success("방문예약 신청이 되었습니다.")
 
@@ -67,30 +58,22 @@ def consult_ui():
         with st.expander("문의하기", expanded=True):
             with st.form("inquiry_form", clear_on_submit=True):
                 col1, col2 = st.columns(2)
-                with col1:
-                    name = st.text_input("이름", key="inq_name")
-                with col2:
-                    phone = st.text_input("전화번호", key="inq_phone")
-
+                name = col1.text_input("이름", key="inq_name")
+                phone = col2.text_input("전화번호", key="inq_phone")
                 col3, col4 = st.columns(2)
-                with col3:
-                    date = st.date_input("문의 날짜", key="inq_date", disabled=True)
-                with col4:
-                    time = st.time_input("문의 시간", key="inq_time", disabled=True)
+                with col3 :
+                    st.date_input("문의 날짜", key="inq_date", disabled=True)
+                with col4 :
+                    st.time_input("문의 시간", key="inq_time", disabled=True)
 
-                content = st.text_area("문의 내용", key="inq_content")
+                content = st.text_area("문의 내용", key="inq_content") or "-"
 
-                if content is None:
-                    content = "-"
-
-                submitted = st.form_submit_button("문의하기")
-
-                if submitted:
+                if st.form_submit_button("문의하기"):
                     new_data = {
                         "이름": name,
                         "전화번호": phone,
-                        "상담날짜": date.strftime("%Y-%m-%d"),
-                        "상담시간": time.strftime("%H:%M"),
+                        "상담날짜": datetime.today().strftime("%Y-%m-%d"),
+                        "상담시간": datetime.today().strftime("%H:%M"),
                         "요청사항": content,
                         "담당직원": "홍길동",
                         "완료여부": 0,
@@ -100,16 +83,10 @@ def consult_ui():
                         "고객피드백": "-",
                         "목적": "문의"
                     }
-
-                    df_path = "data/consult_log.csv"
-                    if os.path.exists(df_path):
-                        df = pd.read_csv(df_path)
-                        df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
-                    else:
-                        df = pd.DataFrame([new_data])
-
+                    df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
                     df.to_csv(df_path, index=False)
                     st.success("문의가 접수되었습니다.")
+
 
     st.markdown("---")
     consult_list, spacer1, consult_true, spacer2, consult_visit, spacer3 ,consult_visit_True = st.columns([0.5, 0.02, 0.5, 0.02, 0.5, 0.02, 0.5])
@@ -195,33 +172,35 @@ def consult_ui():
 
             st.markdown("</div>", unsafe_allow_html=True)
 
-        with consult_true:
-            st.markdown("##### 답변 완료 ")
-            done_df = df[df["완료여부"] == True]
-            total_done_pages = (len(done_df) - 1) // per_page + 1
-            start = st.session_state["done_page"] * per_page
-            end = start + per_page
-            done_df_page = done_df.iloc[start:end]
+    with consult_true:
+        st.markdown("##### 답변 완료 ")
+        done_df = df[df["완료여부"] == True]
+        total_done_pages = (len(done_df) - 1) // per_page + 1
+        start = st.session_state["done_page"] * per_page
+        end = start + per_page
+        done_df_page = done_df.iloc[start:end]
 
-            for idx, row in done_df_page.iterrows():
-                st.markdown(f"""
-                <div style='padding:6px 10px; border-bottom:1px solid #ddd;'>
-                <b>성명:</b> {mask_name(row['이름'])}<br>
-                <b>요청사항:</b> {row['요청사항']}<br>
-                <b>진행상태:</b> 답변 완료
-                </div>
-                """, unsafe_allow_html=True)
+        for idx, row in done_df_page.iterrows():
+            st.markdown(f"""
+            <div style='padding:6px 10px; border-bottom:1px solid #ddd;'>
+            <b>성명:</b> {mask_name(row['이름'])}<br>
+            <b>요청사항:</b> {row['요청사항']}<br>
+            <b>진행상태:</b> 답변 완료
+            </div>
+            """, unsafe_allow_html=True)
 
-                with st.expander("내용확인 및 삭제", expanded=False):
-                    with st.form(f"view_done_{idx}"):
-                        input_name = st.text_input("이름 확인", key=f"done_name_{idx}")
-                        input_phone = st.text_input("전화번호 확인", key=f"done_phone_{idx}")
-                        col_open, col_delete = st.columns([1, 1])
-                        with col_open:
-                            open_clicked = st.form_submit_button("열기")
-                        with col_delete:
-                            delete_clicked = st.form_submit_button("삭제")
+            with st.expander("내용확인 및 삭제", expanded=False):
+                with st.form(f"view_done_{idx}"):
+                    input_name = st.text_input("이름 확인", key=f"done_name_{idx}")
+                    input_phone = st.text_input("전화번호 확인", key=f"done_phone_{idx}")
+                    col_open, col_delete = st.columns([1, 1])
+                    with col_open:
+                        open_clicked = st.form_submit_button("열기")
+                    with col_delete:
+                        delete_clicked = st.form_submit_button("삭제")
 
+                    # 폼 제출 후 처리
+                    if open_clicked or delete_clicked:
                         if input_name.strip() == str(row.get("이름", "")).strip() and input_phone.strip() == str(row.get("전화번호", "")).strip():
                             if open_clicked:
                                 st.info(f"**상담내용:** {row['요청사항']}")
@@ -245,32 +224,32 @@ def consult_ui():
                                 st.success("삭제되었습니다.")
                                 st.rerun()
                         else:
-                            if open_clicked or delete_clicked:
-                                st.warning("정보가 일치하지 않습니다.")
+                            st.warning("정보가 일치하지 않습니다.")
 
-            st.markdown("<div style='text-align:center;'>", unsafe_allow_html=True)
-            page_buttons = st.columns(7)
+        # 페이지네이션 하단 버튼
+        st.markdown("<div style='text-align:center;'>", unsafe_allow_html=True)
+        page_buttons = st.columns(7)
 
-            with page_buttons[0]:
-                if st.button("«", key="done_first"):
-                    st.session_state["done_page"] = 0
+        with page_buttons[0]:
+            if st.button("«", key="done_first"):
+                st.session_state["done_page"] = 0
+                st.rerun()
+
+        start_page = max(0, st.session_state["done_page"] - 2)
+        end_page = min(total_done_pages, start_page + 5)
+
+        for i, page_num in enumerate(range(start_page, end_page)):
+            with page_buttons[i + 1]:
+                if st.button(f"{page_num + 1}", key=f"done_page_{page_num}"):
+                    st.session_state["done_page"] = page_num
                     st.rerun()
 
-            start_page = max(0, st.session_state["done_page"] - 2)
-            end_page = min(total_done_pages, start_page + 5)
+        with page_buttons[6]:
+            if st.button("»", key="done_last"):
+                st.session_state["done_page"] = total_done_pages - 1
+                st.rerun()
 
-            for i, page_num in enumerate(range(start_page, end_page)):
-                with page_buttons[i + 1]:
-                    if st.button(f"{page_num + 1}", key=f"done_page_{page_num}"):
-                        st.session_state["done_page"] = page_num
-                        st.rerun()
-
-            with page_buttons[6]:
-                if st.button("»", key="done_last"):
-                    st.session_state["done_page"] = total_done_pages - 1
-                    st.rerun()
-
-            st.markdown("</div>", unsafe_allow_html=True)
+        st.markdown("</div>", unsafe_allow_html=True)
 
         with consult_visit:
             st.markdown("##### 방문 신청")
